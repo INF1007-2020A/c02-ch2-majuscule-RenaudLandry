010429276f1d6610491d25797de3bb79e49e097a
RenaudLandry <renaud.landry@hotmail.com>
2020-09-12 20:01:04 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
-    
+    distance ebntre min et maj
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
