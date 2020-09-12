0a828c232f3a73926e7cfcb09e652e085299b01f
RenaudLandry <renaud.landry@hotmail.com>
2020-09-12 20:04:00 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,7 +5,7 @@ def majuscule(mot):
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
-        lettre= ord(lettre)- distance entre min et maj
+        lettre= ord(lettre)- distance_entre_min_et_maj
         resultat += chr(lettre)
     return resultat
 
